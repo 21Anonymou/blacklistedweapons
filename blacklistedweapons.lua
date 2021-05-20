5ff267d20eb4e2e16a88f84d3165cecd40c48751
Mr Kostas <76447377+21Anonymou@users.noreply.github.com>
2021-05-20 21:26:52 +0300

    Add files via upload

---
+++ blacklistedweapons.lua
@@ -0,0 +1,42 @@
+-- CONFIG --
+
+-- Edw mporeis na grapseis ta opla ta opoia 8es na einai sthn maurh lista!
+weaponblacklist = {
+	"WEAPON_MINIGUN",
+	"WEAPON_HEAVYPISTOL"
+}
+
+
+disableallweapons = false
+
+-- CODE --
+
+Citizen.CreateThread(function()
+	while true do
+		Wait(1)
+
+		playerPed = GetPlayerPed(-1)
+		if playerPed then
+			nothing, weapon = GetCurrentPedWeapon(playerPed, true)
+
+			if disableallweapons then
+				RemoveAllPedWeapons(playerPed, true)
+			else
+				if isWeaponBlacklisted(weapon) then
+					RemoveWeaponFromPed(playerPed, weapon)
+					sendForbiddenMessage("This weapon is blacklisted!(Dont spawn it again!)")
+				end
+			end
+		end
+	end
+end)
+
+function isWeaponBlacklisted(model)
+	for _, blacklistedWeapon in pairs(weaponblacklist) do
+		if model == GetHashKey(blacklistedWeapon) then
+			return true
+		end
+	end
+
+	return false
+end
